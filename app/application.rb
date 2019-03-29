ffdbdfeb1f5a0c089c7a6aae68281a76b2a57969
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 15:46:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
       if req.path.match(/items/)
         item_name = req.path.split("/items/").last
-        binding.pry
+        
         @@items.each do |item|
           if item.name = item_name
             resp.write "#{item.name}: $#{item.price}"
