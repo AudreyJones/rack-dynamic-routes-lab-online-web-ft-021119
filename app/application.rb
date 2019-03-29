538606f02ddf9e51c1e437c3a6e9a9e0cb04a55f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 15:49:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
           if item.name = item_name
             resp.write "Item not found"
             resp.status 400
-            
+
           else
             # binding.pry
             resp.write "#{item.name}: $#{item.price}\n"
