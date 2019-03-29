ed0753174bc45a1f6f162549e91227b049e18cf0
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 15:42:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,12 +9,12 @@ class Application
         binding.pry
         @@items.include?(item_name)
         @@items.each do {|item|
-          
 
-          
+
+
           if item.name = item_name
             resp.write "#{item.name}: $#{item.price}"
-          if item.name != 
+          if item.name !=
           end
           }
 
